c22c7f77131a4a3bb09935eefabd0bfe3a9436db
XFour <837065279@qq.com>
2019-11-10 15:42:25 +0800

    邻接表创建 2.2

--- 02-Graph-Basics/03-Adjacency-Matrix/src/AdjMatrix.java
+++ 02-Graph-Basics/03-Adjacency-Matrix/src/AdjMatrix.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -20,17 +21,68 @@ public class AdjMatrix {
         try {
             Scanner scanner = new Scanner(file);
             V = scanner.nextInt();
+            if (V < 0) {
+                throw new IllegalArgumentException("V must be non-negative");
+            }
             adj = new int[V][V];
             E = scanner.nextInt();
+            if (E < 0) {
+                throw new IllegalArgumentException("E must be non-negative");
+            }
             for (int i = 0; i < E; i++) {
                 int a = scanner.nextInt();
+                validateVertex(a);
                 int b = scanner.nextInt();
+                validateVertex(b);
+                if (a == b) {
+                    throw new IllegalArgumentException("Self loop is Detected");
+                }
+                if (adj[a][b] == 1) {
+                    throw new IllegalArgumentException("Parallel Edges is Detected");
+                }
                 adj[a][b] = 1;
                 adj[b][a] = 1;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex" + v + "is invalid");
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    //获取与传入顶点邻接的所有顶点
+    public ArrayList<Integer> adj(int v) {
+        validateVertex(v);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (adj[V][i] == 1) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    //获取传入顶点的度
+    public int degree(int v) {
+        return adj(v).size();
+    }
+
+    public boolean hasEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
     }
 
     @Override
